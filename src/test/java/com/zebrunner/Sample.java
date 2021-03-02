f2663f550d7908eb45ce3b9964fb9f8451ce63af
edovnar <edovnar@solvd.com>
2021-03-02 14:43:32 +0300

    add sleep to test

--- src/test/java/com/zebrunner/Sample.java
+++ src/test/java/com/zebrunner/Sample.java
@@ -4,15 +4,18 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Sample {
 
     @Test
-    public void test() {
+    public void test() throws InterruptedException {
+
         Map<String, String> envVars = new TreeMap<>(System.getenv());
         envVars.forEach((key, value) -> System.out.println(key + ": " + value));
+        TimeUnit.SECONDS.sleep(15);
         assertEquals(true, true);
     }
 
